5406b8f71e0f0edd455ea1490b811935b8b115ba
815_PAL ALOKESH SUDIP <psudip22ecs@student.mes.ac.in>
2025-10-10 14:36:18 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1032829495-68f66",
+  "appId": "1:660435607937:web:96005aff25cf2c67288880",
+  "apiKey": "AIzaSyBgdR0PT9tBK9yjj3E7_R3wLlpfZcw1lFM",
+  "authDomain": "studio-1032829495-68f66.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "660435607937"
+};
