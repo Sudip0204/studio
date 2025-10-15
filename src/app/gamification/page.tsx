e27c7992bb530ae39ac937f97531e8c3078338fd
815_PAL ALOKESH SUDIP <psudip22ecs@student.mes.ac.in>
2025-10-15 21:43:34 +0000

    I want leaderboard in right side of the game window.

--- src/app/gamification/page.tsx
+++ src/app/gamification/page.tsx
@@ -1,8 +1,6 @@
 
 'use client';
 
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Trophy } from "lucide-react";
 import { Leaderboard } from "@/components/gamification/leaderboard";
 import dynamic from 'next/dynamic';
 import { Skeleton } from "@/components/ui/skeleton";
@@ -12,7 +10,7 @@ const EcoSnakeGame = dynamic(
   { 
     ssr: false,
     loading: () => (
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center justify-center flex-1 p-4">
         <Skeleton className="w-full max-w-lg h-16 mb-4" />
         <Skeleton style={{ width: 480, height: 480 }} className="rounded-lg" />
         <Skeleton className="w-full max-w-lg h-6 mt-4" />
@@ -23,10 +21,14 @@ const EcoSnakeGame = dynamic(
 
 export default function GamificationPage() {
   return (
-    <div className="w-full">
-        <EcoSnakeGame />
-        <div className="container mx-auto py-12 px-4">
-            <Leaderboard />
+    <div className="container mx-auto py-12 px-4">
+        <div className="grid lg:grid-cols-3 gap-8 items-start">
+            <div className="lg:col-span-2">
+                <EcoSnakeGame />
+            </div>
+            <div className="lg:col-span-1">
+                <Leaderboard />
+            </div>
         </div>
     </div>
   );
