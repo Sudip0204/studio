213f512a1bebdf4b4dada41d7e6d6c75c53ab9ac
815_PAL ALOKESH SUDIP <psudip22ecs@student.mes.ac.in>
2025-11-07 17:59:46 +0000

    "FirebaseError: Firebase Storage: No default bucket found. Did you set t

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:660435607937:web:96005aff25cf2c67288880",
   "apiKey": "AIzaSyBgdR0PT9tBK9yjj3E7_R3wLlpfZcw1lFM",
   "authDomain": "studio-1032829495-68f66.firebaseapp.com",
+  "storageBucket": "studio-1032829495-68f66.appspot.com",
   "measurementId": "",
   "messagingSenderId": "660435607937"
 };
